dbe1f37d8952f252844025200f70397e26db387e
dwm0816 <dwm0816@gmail.com>
2018-02-24 18:02:18 -0800

    Added mobile client detection and outcomes, fixed a few things.

--- jsjq/news.js
+++ jsjq/news.js
@@ -3,7 +3,7 @@ $('#news').append('<h4 class="storyTitle">A brand new website! Again!</hr>')
 $('#news').append('<p class="storyContent">After a bit of work and discusion, we have ultimately decided to switch to a more custom approach to our website.</p>')
 $('#news').append('<hr>')
 $('#news').append('<p class="author">Author: David McKissick</p>')
-$('#news').append('<p class="date">08/15/17</p>')
+$('#news').append('<p class="date">02/24/18</p>')
 
 //---------------------------------------------
 $('#news').append('<br><hr><br>')
