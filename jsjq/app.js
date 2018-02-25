401795b048c8703fe837f36c7a5ab0e29a7b3ac9
dwm0816 <dwm0816@gmail.com>
2018-02-25 13:49:29 -0800

    Active link coloration

--- jsjq/app.js
+++ jsjq/app.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         }
     });
 });
-
+//--
 //---------------------------
 $('#core').prepend('<div class="header">')
 $('.header').append('<div id="logoHold">')
