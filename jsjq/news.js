bc01d0ac34f4338d981379d32a64fc96ce0c0bc9
dwm0816 <dwm0816@gmail.com>
2018-02-24 16:21:31 -0800

    Some new css, a hyper basic news feed, little bit of tweak, news.js

---
+++ jsjq/news.js
@@ -0,0 +1,15 @@
+//ALWAYS PUT NEW STORIES ON TOP. MAXIMUM OF 10 ACTIVE STORIES.
+$('#news').append('<h4 class="storyTitle">A brand new website! Again!</hr>')
+$('#news').append('<p class="storyContent">After a bit of work and discusion, we have ultimately decided to switch to a more custom approach to our website.</p>')
+$('#news').append('<hr>')
+$('#news').append('<p class="author">Author: David McKissick</p>')
+$('#news').append('<p class="date">08/15/17</p>')
+
+//---------------------------------------------
+$('#news').append('<br><hr><br>')
+//---------------------------------------------
+$('#news').append('<h4 class="storyTitle">A brand new website!</hr>')
+$('#news').append('<p class="storyContent">We have officially launched the new website!Here you will find various resources and materials, update frequently, on sarcoidosis, the causes, and the symptoms and treatments for sarcoidosis.Feel free to sign up on our membership page for invitations to events, newsletters and more!</p>')
+$('#news').append('<hr>')
+$('#news').append('<p class="author">Author: Skybrook Foundation</p>')
+$('#news').append('<p class="date">08/15/17</p>')
