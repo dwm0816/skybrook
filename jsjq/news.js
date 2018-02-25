be095b03e99a71a95943d2a6fde6a5ac057cef99
dwm0816 <dwm0816@gmail.com>
2018-02-25 14:28:44 -0800

    New news system, added a secondary app.js to handle news pages

--- jsjq/news.js
+++ jsjq/news.js
@@ -1,15 +1,26 @@
-//ALWAYS PUT NEW STORIES ON TOP. MAXIMUM OF 10 ACTIVE STORIES.
-$('#news').append('<h4 class="storyTitle">A brand new website! Again!</hr>')
-$('#news').append('<p class="storyContent">After a bit of work and discusion, we have ultimately decided to switch to a more custom approach to our website.</p>')
-$('#news').append('<hr>')
-$('#news').append('<p class="author">Author: David McKissick</p>')
-$('#news').append('<p class="date">02/24/18</p>')
+$('#news').append('<h4 class="storyTitle">Gearing up.</hr>')
+$('#news').append('<p class="snippet">After months of preparation, we are nearly ready to launch!</p>')
+$('#news').append('<a href="news/22518.html" class="readMore">Read more...</p>')
 
-//---------------------------------------------
-$('#news').append('<hr><br>')
-//---------------------------------------------
-$('#news').append('<h4 class="storyTitle">A brand new website!</hr>')
-$('#news').append('<p class="storyContent">We have officially launched the new website!Here you will find various resources and materials, update frequently, on sarcoidosis, the causes, and the symptoms and treatments for sarcoidosis.Feel free to sign up on our membership page for invitations to events, newsletters and more!</p>')
 $('#news').append('<hr>')
-$('#news').append('<p class="author">Author: Skybrook Foundation</p>')
-$('#news').append('<p class="date">08/15/17</p>')
+
+$('#news').append('<h4 class="storyTitle">A brand new website!</hr>')
+$('#news').append('<p class="snippet">We have officially launched a new website!</p>')
+$('#news').append('<a href="news/81517.html" class="readMore">Read more...</p>')
+
+
+// //ALWAYS PUT NEW STORIES ON TOP. MAXIMUM OF 10 ACTIVE STORIES.
+// $('#news').append('<h4 class="storyTitle">lA brand new website! Again!</hr>')
+// $('#news').append('<p class="storyContent">After a bit of work and discusion, we have ultimately decided to switch to a more custom approach to our website.</p>')
+// $('#news').append('<hr>')
+// $('#news').append('<p class="author">Author: David McKissick</p>')
+// $('#news').append('<p class="date">02/24/18</p>')
+//
+// //---------------------------------------------
+// $('#news').append('<hr><br>')
+// //---------------------------------------------
+// $('#news').append('<h4 class="storyTitle">A brand new website!</hr>')
+// $('#news').append('<p class="storyContent">We have officially launched the new website!Here you will find various resources and materials, update frequently, on sarcoidosis, the causes, and the symptoms and treatments for sarcoidosis.Feel free to sign up on our membership page for invitations to events, newsletters and more!</p>')
+// $('#news').append('<hr>')
+// $('#news').append('<p class="author">Author: Skybrook Foundation</p>')
+// $('#news').append('<p class="date">08/15/17</p>')
